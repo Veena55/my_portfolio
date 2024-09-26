d7ae5fa887d1d0e80d16a85f9aed97dcc7cb6198
VEENA SURYAPRAKASH RAO <veenasrao5@gmail.com>
2024-09-26 10:23:07 +0530

    project-animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,9 @@ import projects from "../assets/images/sections/projects.png";
 import { LiaLinkSolid } from 'react-icons/lia';
 import gsap from 'gsap';
 import contactme from '../assets/images/sections/contactme.png';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Projects = () => {
     useLayoutEffect(() => {
@@ -41,19 +44,49 @@ const Projects = () => {
             parentMove.addEventListener('mouseleave', () => resetAnimation(index));
         });
 
+        // ScrollTrigger for projectTitle rotation
+        const titleRotation = gsap.to(".projectTitle", {
+            rotate: 360,
+            ease: "circ.inOut",
+            duration: 5,
+            scrollTrigger: {
+                trigger: ".projectTitle",
+                start: "top 90%",
+                end: "top 10%",
+                scrub: true,
+                toggleActions: "play none none none",
+            },
+        });
+
+        // const imgRotation = gsap.to(".projectTitleImg", {
+        //     rotate: -360,
+        //     ease: "circ.inOut",
+        //     duration: 5,
+        //     scrollTrigger: {
+        //         trigger: ".projectTitleImg",
+        //         start: "top 90%",
+        //         end: "top 10%",
+        //         scrub: true,
+        //         toggleActions: "play none none none",
+        //     },
+        // });
+
         // Cleanup event listeners on unmount
         return () => {
             Array.from(parentBoxes).forEach((parentMove, index) => {
                 parentMove.removeEventListener('mousemove', (e) => moveOnProject(e, index));
                 parentMove.removeEventListener('mouseleave', () => resetAnimation(index));
             });
+            gsap.killTweensOf(".projectTitle");
+            gsap.killTweensOf(".projectTitleImg");
         };
     }, []);
 
+
     return (
-        <div className="py-20 mx-auto flex gap-10 justify-around items-center bg">
-            <div>
-                <img src={projects} className="mx-auto w-80" alt="education" />
+        <div className="py-20 mx-auto flex gap-10 justify-around items-center bg projects">
+            <div className='projectTitle outline-dashed outline-4 outline-offset-8 outline-themeDarkColor rounded-full sticky top-0'>
+                <img src={projects} className="mx-auto w-52 h-52 rounded-full shadow-md object-contain border border-themeDarkColor projectTitleImg" alt="education" />
             </div>
             <div className="w-[50%] relative z-50">
                 {/** Project Cards */}
@@ -85,20 +118,17 @@ const projectData = [
     {
         title: 'Resume Builder',
         link: 'https://github.com/Veena55/resume_builder_frontend',
-        description:
-            'Authentication – Google & local Authentication Protected Routes – Client side as well as Server side.',
+        description: 'Authentication – Google & local Authentication Protected Routes – Client side as well as Server side.',
     },
     {
         title: 'NOVA Banking',
         link: 'https://github.com/Veena55/resume_builder_frontend',
-        description:
-            'This is the group project& my role was to build API, enhance UI using Tailwind CSS,basic security to api(s) like to check customer auathenication on each request & API Integration in React. Customer can register and login securly and create account. Can able to create Fixed Deposit and Recurring Deposit accounts & able to see recent transactions, customer profile. Only verified customer’s email can able to create account which is done by mail verification. Customer can make (dummy) transactionsas well.',
+        description: 'This is the group project& my role was to build API, enhance UI using Tailwind CSS,basic security to api(s) like to check customer auathenication on each request & API Integration in React. Customer can register and login securly and create account. Can able to create Fixed Deposit and Recurring Deposit accounts & able to see recent transactions, customer profile. Only verified customer’s email can able to create account which is done by mail verification. Customer can make (dummy) transactionsas well.',
     },
     {
         title: 'Library Management',
         link: 'https://github.com/Veena55/fe-library-management',
-        description:
-            'Can register students, books and track the records of book and student in library.',
+        description: 'Can register students, books and track the records of book and student in library.',
     },
 ];
 
